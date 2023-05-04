8cde97be416181d05932670f4a41717d9b883625
Erick <thelittlebotengineer@gmail.com>
2023-05-04 09:21:07 -0700

    Update 5.4.2023.1a

--- public/Assets/js/about.js
+++ public/Assets/js/about.js
@@ -1,20 +1,28 @@
 window.addEventListener('scroll', function() {
-    var divElements = [
-        {id: 'Contact', navbarSelector: '.topnav .item.left'},
-        {id: 'Signup', navbarSelector: '.topnav .item:nth-child(2)'},
-        {id: 'Calendar', navbarSelector: '.topnav .item:nth-child(3)'},
-        {id: 'Board', navbarSelector: '.topnav .item:nth-child(4)'},
-        {id: 'About', navbarSelector: '.topnav .item:nth-child(5)'},
-        {id: 'Home', navbarSelector: '.topnav .item.right'}
-    ];
-    divElements.forEach(function(divElement) {
-        var element = document.getElementById(divElement.id);
-        var navbarElement = document.querySelector(divElement.navbarSelector);
-        var rect = element.getBoundingClientRect();
-        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-            navbarElement.classList.add('active');
-        } else {
-            navbarElement.classList.remove('active');
-        }
-    });
+    if (this.screen.width <= 719) {
+
+      } else if (this.screen.width > 719) {
+        var divElements = [
+            {id: 'Contact', navbarSelector: '.desktoptopnav .item.left'},
+            {id: 'Signup', navbarSelector: '.desktoptopnav .item:nth-child(2)'},
+            {id: 'Calendar', navbarSelector: '.desktoptopnav .item:nth-child(3)'},
+            {id: 'Board', navbarSelector: '.desktoptopnav .item:nth-child(4)'},
+            {id: 'About', navbarSelector: '.desktoptopnav .item:nth-child(5)'},
+            {id: 'Home', navbarSelector: '.desktoptopnav .item.right'}
+        ];
+        divElements.forEach(function(divElement) {
+            var element = document.getElementById(divElement.id);
+            var navbarElement = document.querySelector(divElement.navbarSelector);
+            var rect = element.getBoundingClientRect();
+            if (rect.top < window.innerHeight && rect.bottom > 0) {
+                navbarElement.classList.add('active');
+            } else {
+                navbarElement.classList.remove('active');
+            }
+        });
+      } else {
+        //console.log("Unknown device");
+      }
+
 });
+
