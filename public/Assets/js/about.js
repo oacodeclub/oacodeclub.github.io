0d60208e27e8c4a44035a9605503117f4eeda267
LilGamerPlayz <92957880+LilGamerPlayz@users.noreply.github.com>
2023-05-06 00:39:22 -0700

    Update 5.6.2023.1a

--- public/Assets/js/about.js
+++ public/Assets/js/about.js
@@ -4,7 +4,25 @@ window.addEventListener("load", async function () {
 
 window.addEventListener('scroll', function () {
   if (this.screen.width <= 719) {
-
+    let divElements = [
+      { id: 'Home', navbarSelector: '.dropdown-content .item.left', Title: 'Contact' },
+      { id: 'About', navbarSelector: '.dropdown-content .item:nth-child(2)', Title: 'Sign Up' },
+      { id: 'Board', navbarSelector: '.dropdown-content .item:nth-child(3)', Title: 'Calendar' },
+      { id: 'Calendar', navbarSelector: '.dropdown-content .item:nth-child(4)', Title: 'The Board' },
+      { id: 'Signup', navbarSelector: '.dropdown-content .item:nth-child(5)', Title: 'About' },
+      { id: 'Contact', navbarSelector: '.dropdown-content .item.right', Title: 'Home' }
+    ];
+    divElements.forEach(function (divElement) {
+      const element = document.getElementById(divElement.id);
+      let navbarElement = document.querySelector(divElement.navbarSelector);
+      let rect = element.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        navbarElement.classList.add('active');
+      } else {
+        navbarElement.classList.remove('active');
+      }
+    });
+    //console.log("Unknown device");
   } else if (this.screen.width > 719) {
     let divElements = [
       { id: 'Contact', navbarSelector: '.desktoptopnav .item.left', Title: 'Contact' },
@@ -29,10 +47,8 @@ window.addEventListener('scroll', function () {
   }
 });
 
-window.onload = function () {
-  var HamburgerSelectionButton = document.getElementById("HamburgerSelectionButton");
-
-  HamburgerSelectionButton.addEventListener("touchstart", function () {
-    document.getElementById('MobileVisibleUl').style = 'display:unset;'
-  });
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropDown() {
+  document.getElementById("MobileVisibleUl").classList.toggle("show");
 }
